51ce0182696462485372a25038fe07705012119c
Adrian Ziegler <adrian.ziegler@student.hpi.de>
2020-05-19 16:51:05 +0200

    Change folder path of cifar dataset

--- configs.py
+++ configs.py
@@ -1,7 +1,7 @@
 save_dir                    = './record/'
 data_dir = {}
 data_dir['CUB']             = './filelists/CUB/'
-data_dir['CIFARFS']         = './filelists/CIFAR-FS/'
+data_dir['CIFARFS']         = './filelists/CIFARFS/'
 data_dir['miniImagenet']    = './filelists/miniImagenet/' 
 data_dir['omniglot']        = './filelists/omniglot/' 
 data_dir['emnist']          = './filelists/emnist/' 
