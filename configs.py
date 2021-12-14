b7f296a446e04a88fa96dd39710edfdc41a19ad8
chan-w <40780153+chan-w@users.noreply.github.com>
2021-12-14 17:30:08 -0600

    Add path to directory used in Colab to config.py. This path must be valid, otherwise the program fails silently.

--- configs.py
+++ configs.py
@@ -1,4 +1,4 @@
-save_dir                    = '/work/newriver/wyharveychen/CloserLookFewShot/'
+save_dir                    = '/content/gdrive/MyDrive/aclafsc/CloserLookFewShot/'
 data_dir = {}
 data_dir['CUB']             = './filelists/CUB/' 
 data_dir['miniImagenet']    = './filelists/miniImagenet/' 
